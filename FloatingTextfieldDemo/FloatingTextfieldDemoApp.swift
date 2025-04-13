3764b6795bcb61b7bb88de7a7027ee4b1d7e2d0f
lburris57 <lburris@mac.com>
2025-04-13 19:11:02 -0400

    Initial Commit

---
+++ FloatingTextfieldDemo/FloatingTextfieldDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingTextfieldDemoApp.swift
+//  FloatingTextfieldDemo
+//
+//  Created by Larry Burris on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct FloatingTextfieldDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FloatingTextfieldDemoTests/FloatingTextfieldDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingTextfieldDemoTests.swift
+//  FloatingTextfieldDemoTests
+//
+//  Created by Larry Burris on 4/13/25.
+//
+
+import Testing
+@testable import FloatingTextfieldDemo
+
+struct FloatingTextfieldDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
