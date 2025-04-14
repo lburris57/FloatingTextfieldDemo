c987f39e600adbae2fb7e5fcc5841e8bd694afd9
lburris57 <lburris@mac.com>
2025-04-13 20:24:54 -0400

    Added verical spacing between fields

--- FloatingTextfieldDemo/ContentView.swift
+++ FloatingTextfieldDemo/ContentView.swift
@@ -4,21 +4,38 @@
 //
 //  Created by Larry Burris on 4/13/25.
 //
-
+import FloatingPromptTextField
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct ContentView: View
+{
+    @Environment(\.colorScheme) var colorScheme
+    
+    @State var title: String = ""
+    @State var description: String = ""
+    @State var comment: String = ""
+
+    var body: some View
+    {
+        VStack(spacing: 20)
+        {
+            FloatingPromptTextField(text: $title, prompt: Text("Title:")
+                .foregroundStyle(colorScheme == .dark ? .gray : .blue))
+            .floatingPromptScale(1.0)
+            
+            FloatingPromptTextField(text: $description, prompt: Text("Description:")
+                .foregroundStyle(colorScheme == .dark ? .gray : .blue))
+            .floatingPromptScale(1.0)
+            
+            FloatingPromptTextField(text: $comment, prompt: Text("Comment:")
+                .foregroundStyle(colorScheme == .dark ? .gray : .blue))
+            .floatingPromptScale(1.0)
         }
         .padding()
     }
 }
 
-#Preview {
+#Preview
+{
     ContentView()
 }
